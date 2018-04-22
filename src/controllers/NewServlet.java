cf19e91f8ab83a2537fe07f7322467c9bb188c0e
telaboat <telaboat@gmail.com>
2018-04-22 15:46:45 +0900

    Modify Index/NewServlet

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -2,7 +2,7 @@ package controllers;
 
 import java.io.IOException;
 
-import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,14 +10,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
-import utils.DBUtil;
 
 /**
  * Servlet implementation class NewServlet
  */
 @WebServlet("/new")
 public class NewServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
@@ -27,26 +26,15 @@ public class NewServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+        /**
+         * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+         */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManager em = DBUtil.createEntityManager();
-        em.getTransaction().begin();
+        request.setAttribute("_token", request.getSession().getId());
+        request.setAttribute("message", new Task());
 
-        Task m = new Task();
-
-
-        String content = "会議";
-        m.setContent(content);
-
-        em.persist(m);
-        em.getTransaction().commit();
-
-        response.getWriter().append(Integer.valueOf(m.getId()).toString());
-
-        em.close();
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+        rd.forward(request, response);
     }
-
 
 }
